a6254b035b7749eaf089a538031980b1818630bc
Marko Klemetti <marko.klemetti@gmail.com>
2020-11-18 21:51:21 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# find-local-ips
+# Find Local IPs
+
+The purpose of this tool is to find out Raspberry Pi address when it is connected to the same network with your computer.
+
+## Usage (Linux Docker)
+
+```bash
+docker run --network host mrako/find-local-ips [ip address]
+```
+
+## Usage (Node)
+
+```bash
+node index.js [ip address]
+```
+
+## Building
+
+```bash
+docker build -t mrako/find-local-ips .
+docker push mrako/find-local-ips
+```
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const ping = require('ping');
 const ip = require('ip');
 
+const localIp = process.argv[2] || ip.address();
+
 const checkIp = async (host, port = 22) => {
   const res = await ping.promise.probe(host);
+  console.log(host);
 
   if (res.alive) {
     console.log(`alive: ${host}`);
@@ -10,8 +13,6 @@ const checkIp = async (host, port = 22) => {
 }
 
 (async () => {
-  const localIp = ip.address();
-
   const addressBlock = localIp.substring(0, localIp.lastIndexOf('.'));
 
   const ipBlock = [...Array(254).keys()];
