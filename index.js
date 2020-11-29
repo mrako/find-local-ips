645c8861985c1b042ee084304fddee3b0c8b003f
Marko Klemetti <marko.klemetti@gmail.com>
2020-11-29 19:22:46 +0200

    Add MAC lookup

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const ping = require('ping');
+const arp = require('@network-utils/arp-lookup');
 const ip = require('ip');
+const oui = require('oui');
+const ping = require('ping');
 
 const localIp = process.argv[2] || ip.address();
+
+const macLookup = async (host) => {
+  const mac = await arp.toMAC(host);
+  const lookup = oui(mac);
+
+  if (lookup) {
+    return lookup.split('\n')[0];
+  }
+  return mac;
+};
 
 const checkIp = async (host, port = 22) => {
   const res = await ping.promise.probe(host);
 
   if (res.alive) {
-    console.log(`alive: ${host}`);
+    console.log(`${host} - ${await macLookup(host)}`);
   }
-}
+};
 
 (async () => {
   const addressBlock = localIp.substring(0, localIp.lastIndexOf('.'));
