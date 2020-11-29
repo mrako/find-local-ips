ec002e4b2f5df2df642aa3d0d1099222b5a403bd
Marko Klemetti <marko.klemetti@gmail.com>
2020-11-29 19:44:26 +0200

    Add cli and bin scripts

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const ip = require('ip');
 const oui = require('oui');
 const ping = require('ping');
 
-const localIp = process.argv[2] || ip.address();
-
-const macLookup = async (host) => {
+const lookupMacAddress = async (host) => {
   const mac = await arp.toMAC(host);
   const lookup = oui(mac);
 
@@ -15,18 +13,22 @@ const macLookup = async (host) => {
   return mac;
 };
 
-const checkIp = async (host, port = 22) => {
+const checkIpAndPrintInfo = async (host, port = 22) => {
   const res = await ping.promise.probe(host);
 
   if (res.alive) {
-    console.log(`${host} - ${await macLookup(host)}`);
+    console.log(`${host} - ${await lookupMacAddress(host)}`);
   }
 };
 
-(async () => {
+const findLocalIps = async (args) => {
+  const localIp = args[2] || ip.address();
+
   const addressBlock = localIp.substring(0, localIp.lastIndexOf('.'));
 
   const ipBlock = [...Array(254).keys()];
 
-  ipBlock.map(i => checkIp(`${addressBlock}.${i + 1}`));
-})();
+  ipBlock.map(i => checkIpAndPrintInfo(`${addressBlock}.${i + 1}`));
+}
+
+module.exports = { findLocalIps };
